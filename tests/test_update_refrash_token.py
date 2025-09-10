a92a3188bd5a4aa1bfea3f75afd7cb567a435b96
777Chara777 <sans777sans@gmail.com>
2025-09-10 03:37:15 +0300

    Added VSCode settings, refresh token update test, server.py changed

---
+++ tests/test_update_refrash_token.py
@@ -0,0 +1,19 @@
+import unittest
+import threading
+
+from utils import Logger
+from main import ServerApp
+
+class TestUpdateRefreshToken(unittest.TestCase):
+
+    def __init__(self, methodName = "runTest"):
+        super().__init__(methodName)
+
+        self.logger = Logger(self.__class__.__name__)
+
+
+    def test_update_token(self):
+        server_thread = threading.Thread(target=ServerApp().run, daemon=True)
+        self.logger.info("Server starting...", )
+        server_thread.start()
+        self.logger.info("Server Done")
